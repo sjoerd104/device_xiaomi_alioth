a676cb4247817c62c25c0d1cdf762b251b209fb5
Sjoerd <sjoerdnoob@instance-1.europe-west1-b.c.lustrous-aleph-364515.internal>
2022-10-06 13:38:54 +0000

    Revert "Alioth: Updatable APEX from Google Pixels"
    This reverts commit ca53e79c441505c7252b00b8cefd3a1ef87f7623.

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -12,10 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common VoidUI stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Inherit Google Pixel Updatable APEX
-TARGET_SHIP_PREBUILT_APEX := true
-$(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
-
 # Inherit Google Pixel Carrier Settings
 TARGET_INCLUDE_CARRIER_SETTINGS := true
 
